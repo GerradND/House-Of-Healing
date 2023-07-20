4b50688d8c809085d9407f26a8b6de57f120d3a9
GerradND <gerrad1106@gmail.com>
2023-07-20 13:37:51 +0700

    Add NavBar component on App

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NavBar />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/Navbar.vue";
 
 export default {
     name: "App",
     components: {
-        HelloWorld,
+        NavBar,
     },
 };
 </script>
@@ -20,7 +19,51 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+}
+
+:root {
+    --bs-green: #5c745c;
+    --bs-black1: #000;
+    --bs-black2: #5c745c;
+    --bs-white1: #fff;
+    --bs-white2: #f1f2eb;
+    --bs-brown: #978061;
+    --bs-grey: #aba697;
+}
+
+html {
+    box-sizing: border-box;
+    font-size: 16px;
+}
+
+*,
+*:before,
+*:after {
+    box-sizing: inherit;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ol,
+ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+}
+
+ol,
+ul {
+    list-style: none;
+}
+
+img {
+    max-width: 100%;
+    height: auto;
 }
 </style>
